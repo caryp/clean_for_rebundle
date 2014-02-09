be1d9f8e4c9b659100e658b72a6dcf27d57891d9
caryp <cary@rightscale.com>
2014-02-08 21:50:40 -0800

    added extra line ending

--- run.sh
+++ run.sh
@@ -30,3 +30,4 @@ tar xvzf clean_for_rebundle.tar.gz --strip-components=1
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
